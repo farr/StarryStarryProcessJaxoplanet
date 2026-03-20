cacbc169e6ea25b89cccf2593aba82ce738e1002
Will M. Farr <will.farr@ligo.org>
2026-03-19 22:56:34 -0400

    Sequences of Gamma functions that are also useful.

--- utils.py
+++ utils.py
@@ -2,12 +2,6 @@ import jax.numpy as jnp
 import jax.lax as jlx
 import jax.scipy.special as jss
 
-def qphil_array(l, alpha, beta):
-    """Compute `(q^l_phi)_i` for `i = 0, 1, ..., 2*l`."""
-    i = jnp.arange(2 * l + 1, step=2)
-    init_factor = jnp.exp(jlx.lgamma(alpha) + jlx.lgamma(l + beta) - jlx.lgamma(l + alpha + beta)) * jss.hyp2f1(0.0, alpha, l+alpha+beta,-1.0)
-
-
 def Fam(F, Fcp, l, alpha, beta, i):
     """Compute `2F1(a-1, b, c, d)` in terms of `2F1(a, b, c, d)` and `2F1(a, b, c+1,
     d)`."""
@@ -65,6 +59,66 @@ def hyp2f1_sequence(l, alpha, beta, imax=None):
         Fcm_ = Fcm(Fam_, F_, l, alpha, beta, i)
         return (Fcm_, Fam_), Fam_
         
+    _, result_evens = jlx.scan(scan_f, init, jnp.arange(imax+1, step=2))
+    result = jnp.zeros(imax+1)
+    result = result.at[0::2].set(result_evens)
+    return result
+
+def gamma_sequence1(l, alpha, beta, imax=None):
+    """Returns a sequence of gamma functions which is zero at odd indices, and even
+    indices `i` given by `Gamma(alpha) * Gamma(l + beta - i/2) / Gamma(l + alpha +
+    beta - i/2)`.
+
+    .. doctest::
+
+        >>> import jax.lax as jlx
+        >>> import jax.numpy as jnp
+        >>> l, alpha, beta = 3, 2.0, 5.0
+        >>> imax = 12
+        >>> result_sequence = gamma_sequence1(l, alpha, beta, imax)
+        >>> expected_sequence = jnp.array([jnp.exp(jlx.lgamma(alpha) + jlx.lgamma(l + beta - i/2) - jlx.lgamma(l + alpha + beta - i/2)) if i % 2 == 0 else 0.0 for i in range(imax+1)])
+        >>> jnp.allclose(result_sequence, expected_sequence)
+        Array(True, dtype=bool)
+    """
+    if imax is None:
+        imax = 2*l
+
+    init = jnp.exp(jlx.lgamma(alpha) + jlx.lgamma(l + beta + 1) - jlx.lgamma(l + alpha + beta + 1))
+
+    def scan_f(state, i):
+        next_state = state * (l + alpha + beta - i/2) / (l + beta - i/2)
+        return (next_state, next_state)
+    
+    _, result_evens = jlx.scan(scan_f, init, jnp.arange(imax+1, step=2))
+    result = jnp.zeros(imax+1)
+    result = result.at[0::2].set(result_evens)
+    return result
+
+def gamma_sequence2(l, alpha, beta, imax=None):
+    """Returns a sequence of gamma functions which is zero at odd indices and at
+    even indices `i` given by `2^l * Gamma((1+i)/2) * Gamma(l + (i-1)/2) /
+    (jnp.pi*Gamma(l+1))`.
+
+    .. doctest::
+
+        >>> import jax.scipy.special as jss
+        >>> import jax.numpy as jnp
+        >>> l, alpha, beta = 3, 2.0, 5.0
+        >>> imax = 6
+        >>> result_sequence = gamma_sequence2(l, alpha, beta, imax)
+        >>> expected_sequence = jnp.array([2**l * jss.gamma((1+i)/2) * jss.gamma(l + (1-i)/2) / (jnp.pi * jss.gamma(l+1)) if i % 2 == 0 else 0.0 for i in range(imax+1)])
+        >>> jnp.allclose(result_sequence, expected_sequence)
+        Array(True, dtype=bool)
+    """
+    if imax is None:
+        imax = 2*l
+
+    init = jss.gamma(-1/2)*jnp.exp(l*jnp.log(2) + jlx.lgamma(l + 3/2) - jnp.log(jnp.pi) - jlx.lgamma(l + 1.0))
+
+    def scan_f(state, i):
+        next_state = state * ((1+i)/2 - 1) / (l + (1-i)/2)
+        return (next_state, next_state)
+    
     _, result_evens = jlx.scan(scan_f, init, jnp.arange(imax+1, step=2))
     result = jnp.zeros(imax+1)
     result = result.at[0::2].set(result_evens)
